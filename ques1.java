1c22015dd5834787776590406fa5f3b4412f1c30
Master-mohit <mohitmansigupta9098@gmail.com>
2024-09-07 18:05:40 +0530

    first commit

---
+++ ques1.java
@@ -0,0 +1,112 @@
+
+public class ques1 {
+     // 1 - Sum of two integers
+     public static void main(String[] args) {
+    //     int a = 10;
+    //     int b = 10;
+    //     System.out.println("sum = " +(a + b));
+
+    // 2- Print the messege along with values and sum....
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Tell me a: ");
+    // int a = sc.nextInt(); // Taking input for 'a' from the user
+    // System.out.println("Tell me b: ");
+    // int b = sc.nextInt(); // Taking input for 'b' from the user
+    // int sum = a + b;
+    // System.out.println("The sum of " + a + " & " + b + " = " + sum);    
+          
+     // 4- Accept the User's name, age and print in following manner
+	       // Ex - Hello Shery, you are 12 years old.                 
+                //  Scanner sc = new Scanner(System.in);
+                //  System.out.println("Type your name:");
+                //  String name = sc.nextLine();
+                //  System.out.println("Type your age:");
+                //  int age = sc.nextInt();
+                //  System.out.println("Hello " + name + ", you are " + age + " years old.");
+         
+                // Swaping variable with third varibale...args
+                // int a = 5; 
+                // int b = 10;
+                // int temp = a;
+                //  a = b;
+                //  b = temp;
+                // System.out.println(a);
+                // System.out.println(b);
+
+     // Swaping variable without third varibale
+        //  Scanner sc = new Scanner(System.in);
+        //     int a = sc.nextInt();
+        //    int b = sc.nextInt();
+       
+        //    a = a + b;
+        //    b = a - b;
+        //    a = a - b;
+        //  System.out.println(a);
+        //  System.out.println(b);
+
+        // Swaping variable without third varibale Bitwise XOR operator...
+    //       Scanner sc = new Scanner(System.in);
+    //    int a = sc.nextInt();
+    //    int b = sc.nextInt();
+    //    a = a ^ b;
+    //    b = a ^ b;
+    //    a = a ^ b;
+    //  System.out.println(a);
+    // System.out.println(b);
+
+    // 7- the length and width of a rectangle. Calculate & print the area and perimiter. 
+    //   Scanner sc = new Scanner(System.in);
+    //    int l = sc.nextInt();
+    //    int w = sc.nextInt();
+    //    System.out.println(l * w);
+    //    System.out.println(2 * (l + w));
+
+    // 8- Accept the parameters and calculate the Compound Interest & print it on
+//      STDOUT (Use Math class methods)
+//   Scanner sc = new Scanner(System.in);
+//         System.out.println("principle");
+//         int p = sc.nextInt(); 
+//        System.out.println("Rate");
+//         int r = sc.nextInt(); 
+//         System.out.println("Time");
+//         int t = sc.nextInt();
+//         System.out.println("Number of interst");
+//         int n = sc.nextInt();
+        
+//       double amount = p * Math.pow((1 + (r / 100) / n), n * t); 
+//      double compount  = amount - p;
+
+// 10 Find surface area of sphere
+
+    //    Scanner sc = new Scanner(System.in);
+    //   System.out.println("tell me radius");
+    //   double radius = sc.nextDouble(); 
+    //   double ans = 4 * Math.PI * Math.pow(radius, 2);
+    //   System.out.println(ans);
+
+//     8- Accept the parameters and calculate the Compound Interest & 
+//     print it on STDOUT (Use Math class methods)
+
+//     		Scanner sc = new Scanner(System.in);
+// 		System.out.println("a");
+// 		double a = sc.nextInt();
+// 		System.out.println("b");
+// 		double b = sc.nextInt();
+// 		System.out.println("c");
+// 		double c = sc.nextInt();
+        
+//       double semipermeter = (a + b + c) / 2;
+      
+//       double totl = Math.sqrt(semipermeter * (semipermeter - a) * 
+//       (semipermeter - b) * (semipermeter - c));
+      
+//    System.out.println(totl);
+
+
+
+             }
+         }
+         
+
+      
+

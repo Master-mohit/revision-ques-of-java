c994ed21d44ad7c791af38cbb491d1c7d78cf038
Master-mohit <mohitmansigupta9098@gmail.com>
2024-10-21 12:22:44 +0530

    INSERTION SORT

--- Sorting.java
+++ Sorting.java
@@ -47,3 +47,22 @@ for(int i =0; i<arr.length; i++){
 for(int i=0; i<arr.length; i++){
 	System.out.print(arr[i] + " ");
 }
+
+
+// ...................................Insertion sort.............................
+
+int[] arr = {29, 10, 14, 37, 13};
+	    
+for(int i=1; i<arr.length; i++){
+	int current = arr[i];
+	int j = i-1;
+	
+	while(j >= 0 && arr[j] > current){
+		arr[j+1] = arr[j];
+		j--;
+	}
+	   arr[j+1] = current;
+}
+for(int i=0; i<arr.length; i++){
+	System.out.print(arr[i]+ " ");
+}

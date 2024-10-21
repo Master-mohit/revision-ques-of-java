0241039ccc574f31b870b30ad6e3caebbf62dfdc
Master-mohit <mohitmansigupta9098@gmail.com>
2024-10-21 11:08:39 +0530

    Selection sort

--- Sorting.java
+++ Sorting.java
@@ -28,3 +28,22 @@ public class Main
 		}
 	}
 }
+
+// ........................SELECTION SORT....................................
+
+int arr [] = {29, 10, 14, 37, 13};
+		
+for(int i =0; i<arr.length; i++){
+	 int min = i;
+	for(int j = i+1; j<arr.length; j++){
+		if(arr[j] < arr[min]){
+			min = j;
+		}
+	}
+	int temp = arr[min];
+	arr[min] = arr[i];
+	arr[i] = temp;
+}
+for(int i=0; i<arr.length; i++){
+	System.out.print(arr[i] + " ");
+}

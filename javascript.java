9dffe214d8a33daf69e8f03989e3d50781eff605
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-11 13:19:01 +0530

    anagram

--- javascript.java
+++ javascript.java
@@ -75,3 +75,32 @@
 // for(const key of map.keys()){
 //    console.log(key + " " + map.get(key) + ' time')
 // }
+
+// 10) ..............First non repating elements..........
+// const arr = "mmwiss";
+// const map = new Map();
+// for(const ch of arr){
+//   map.set(ch, (map.get(ch) || 0) + 1);
+// }
+// for(const key of map.keys()){
+//   if(map.get(key) == 1 ){
+//     console.log(key);
+//     break;
+//   }
+// }
+
+
+// 11) ..............anagram String..................
+// const s1 = "silent";
+// const s2 = "listen";
+// if(s1.length != s2.length){
+//   console.log("not anagram");
+// }
+//   const s3 = s1.split("").sort().join("");
+//   const s4 = s2.split("").sort().join("");      
+//            if(s3 == s4){
+//              console.log("anagram");     
+//            }
+//            else {
+//              console.log("not anagram")
+//            }

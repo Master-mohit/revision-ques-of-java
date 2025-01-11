d99654d5079a53357ca691cb777d95d34564b279
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-11 13:01:27 +0530

    using map

--- javascript.java
+++ javascript.java
@@ -23,8 +23,55 @@
 // const ans = Math.max(...arr);
 // console.log(ans);
 
-// 3) .................Minimum number in array................
+// 4) .................Minimum number in array................
 // const arr = [1,5,3,8,4];
 // const ans = Math.min(...arr);
 // console.log(ans);
 
+// 5) .................Pallindrome string....................
+// const str = "abca";
+// const instance = str;
+// const reverse = str.split("").reverse().join("");
+// if(reverse == instance){
+//   console.log("pallindrome");
+// }
+// else {
+//   console.log("not pallindrome");
+// }
+
+// 6) .................Remove duplicate value.................
+// const arr = [2,3,4,4,2,6,5];
+// const unique = [...new Set(arr)];
+// console.log(unique);
+
+// 7) .................Remove duplicate value..................
+// const arr = [2,3,4,4,2,6,5];
+// const map = new Map();
+// for(const num of arr){
+//   map.set(num , (map.get(num) || 0) + 1);
+// }
+// for(const key of map.keys()){
+//   if(map.get(key) > 1){
+//     console.log(key);
+//   }
+// }
+
+// 8) .................compress number..................
+// const arr = [2,3,4,4,2,6,5];
+// const map = new Map();
+// for(const num of arr){
+//   map.set(num, (map.get(num) || 0) +  1);
+// }
+// for(const key of map.keys()){
+//    console.log(key + " " + map.get(key) + ' time')
+// }
+
+// 9) .................compress String...................
+// const arr = "mohitt";
+// const map = new Map();
+// for(const ch of arr){
+//   map.set(ch, (map.get(ch) || 0) +  1);
+// }
+// for(const key of map.keys()){
+//    console.log(key + " " + map.get(key) + ' time')
+// }

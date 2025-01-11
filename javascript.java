c175c229892646bcd5630fea103afdb801ada0dd
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-11 12:01:36 +0530

    javascript basic questions

---
+++ javascript.java
@@ -0,0 +1,30 @@
+// 1) ..................Reverse String.................
+// const str = "hello";
+// const reverse = str.split("").reverse().join("");
+// console.log(reverse);
+
+// 2) .................Prime number.....................
+// const n = 11;
+// let count =0;
+// for(let i=1; i<=n; i++){
+//   if(n % i == 0){
+//     count++;
+//   }
+// }
+// if(count == 2){
+//     console.log("prime number");
+//   }
+//   else {
+//     console.log("not prime");
+//   }
+
+// 3) .................Maximum number in array................
+// const arr = [1,5,3,8,4];
+// const ans = Math.max(...arr);
+// console.log(ans);
+
+// 3) .................Minimum number in array................
+// const arr = [1,5,3,8,4];
+// const ans = Math.min(...arr);
+// console.log(ans);
+

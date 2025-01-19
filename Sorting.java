55324ddd980ac4a1ff5ea9bfc2e9ec780c342c38
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-19 13:54:38 +0530

    Quick sort

--- Sorting.java
+++ Sorting.java
@@ -121,3 +121,43 @@ public class Main
 	  System.out.println();
 	}
 }
+
+// ..........QICK SORT....................
+import java.util.*;
+public class Main
+{
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1;
+        for(int j=si; j<ei; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+           int temp = arr[i+1];
+           arr[i+1]  = arr[ei];
+           arr[ei] = temp;
+           return i+1;
+    }
+    
+    public static void quicksort(int arr[], int si, int ei){
+        if(si < ei){
+            int pidx = partition(arr , si, ei);
+        quicksort(arr,si, pidx-1);
+        quicksort(arr, pidx+1, ei);
+        }
+    }
+	public static void main(String[] args) {
+		int arr[] = {5,2,7,8,6,9};
+		int n = arr.length;
+		quicksort(arr , 0, n-1);
+// 		print..
+      for(int i=0; i<n; i++){
+          System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+	}
+}

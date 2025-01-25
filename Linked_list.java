83bbd82ed64cdceb3a1dc208e5d32f623e64187e
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-25 11:13:43 +0530

    delete node from middle anywhere

--- Linked_list.java
+++ Linked_list.java
@@ -488,6 +488,50 @@ public class Main
 	}
 }
 
+
+//  ...................middle se kahi se bhi delete node.................
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		
+		int position = 3;
+		  Node temp = head;
+		  
+		  if(temp == null){
+		    System.out.println("null");
+		    return;
+		  }
+		  else {
+		      for(int i=0; i<position-1; i++){
+		          temp = temp.next;
+		      }
+		      temp.next = temp.next.next;
+		  }
+	
+	    temp = head;
+		while(temp != null){
+		    System.out.print(temp.data + "->");
+		    temp = temp.next;
+		}
+		System.out.println("null");
+	}
+}
+
 // ..............................REVERSE LINKED LIST...................................
 // class Node {
 //     int data;

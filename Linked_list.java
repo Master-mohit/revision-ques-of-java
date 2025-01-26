150862867efeeffcf1366ceded1d3cae284945e0
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-26 11:41:08 +0530

    delete middle element in LiskedLIst

--- Linked_list.java
+++ Linked_list.java
@@ -532,6 +532,52 @@ public class Main
 	}
 }
 
+//  ...............delete middle elment of linked list...............
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+}
+}
+public class Main
+{
+public static void main(String[] args) {
+    Node head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(3);
+    head.next.next.next = new Node(4);
+    head.next.next.next.next = new Node(5);
+    
+    Node slow = head;
+    Node fast = head;
+    Node prev = null;
+    
+    while(fast.next != null && fast.next.next != null){
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    if(prev != null){
+        prev.next = prev.next.next;
+    }
+    else {
+        head = head.next;
+    }
+    
+    Node temp =  head;
+    while(temp != null){
+        System.out.print(temp.data + "->");
+        temp = temp.next;
+    }
+    System.out.println("null");
+}
+}
+
 // ..............................REVERSE LINKED LIST...................................
 // class Node {
 //     int data;

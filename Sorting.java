c4442c728b2de03befcb76ac723be1aa220b627d
Master-mohit <mohitmansigupta9098@gmail.com>
2025-05-05 04:21:37 +0530

    commit

--- Sorting.java
+++ Sorting.java
@@ -1,163 +1,163 @@
-// ...................................Bubble sort...................................
+// // ...................................Bubble sort...................................
 
-public class Main
-{
-	public static void main(String[] args) {
-		int [] arr = {76, 46, 3, 65, 2, 69};
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 		int [] arr = {76, 46, 3, 65, 2, 69};
 		
-		for(int i=0; i<arr.length; i++){
+// 		for(int i=0; i<arr.length; i++){
 		    
-		    int count = 0;
+// 		    int count = 0;
 		    
-		    for(int j=0; j<arr.length-1-i; j++){
+// 		    for(int j=0; j<arr.length-1-i; j++){
 		        
-		        if(arr[j] > arr[j + 1 ]){
+// 		        if(arr[j] > arr[j + 1 ]){
 		            
-		            int temp = arr[j];
-		            arr[j] = arr[j + 1];
-		            arr[j + 1] = temp;
-		            count = 1;
-		        }
-		    }
-		    if(count == 0){
-		        break;
-		    }
-		}
-		for(int i=0; i<arr.length; i++){
-		    System.out.print(arr[i] + " ");
-		}
-	}
-}
+// 		            int temp = arr[j];
+// 		            arr[j] = arr[j + 1];
+// 		            arr[j + 1] = temp;
+// 		            count = 1;
+// 		        }
+// 		    }
+// 		    if(count == 0){
+// 		        break;
+// 		    }
+// 		}
+// 		for(int i=0; i<arr.length; i++){
+// 		    System.out.print(arr[i] + " ");
+// 		}
+// 	}
+// }
 
-// ........................SELECTION SORT....................................
+// // ........................SELECTION SORT....................................
 
-int arr [] = {29, 10, 14, 37, 13};
+// int arr [] = {29, 10, 14, 37, 13};
 		
-for(int i =0; i<arr.length; i++){
-	 int min = i;
-	for(int j = i+1; j<arr.length; j++){
-		if(arr[j] < arr[min]){
-			min = j;
-		}
-	}
-	int temp = arr[min];
-	arr[min] = arr[i];
-	arr[i] = temp;
-}
-for(int i=0; i<arr.length; i++){
-	System.out.print(arr[i] + " ");
-}
+// for(int i =0; i<arr.length; i++){
+// 	 int min = i;
+// 	for(int j = i+1; j<arr.length; j++){
+// 		if(arr[j] < arr[min]){
+// 			min = j;
+// 		}
+// 	}
+// 	int temp = arr[min];
+// 	arr[min] = arr[i];
+// 	arr[i] = temp;
+// }
+// for(int i=0; i<arr.length; i++){
+// 	System.out.print(arr[i] + " ");
+// }
 
 
-// ...................................Insertion sort.............................
+// // ...................................Insertion sort.............................
 
-int[] arr = {29, 10, 14, 37, 13};
+// int[] arr = {29, 10, 14, 37, 13};
 	    
-for(int i=1; i<arr.length; i++){
-	int current = arr[i];
-	int j = i-1;
+// for(int i=1; i<arr.length; i++){
+// 	int current = arr[i];
+// 	int j = i-1;
 	
-	while(j >= 0 && arr[j] > current){
-		arr[j+1] = arr[j];
-		j--;
-	}
-	   arr[j+1] = current;
-}
-for(int i=0; i<arr.length; i++){
-	System.out.print(arr[i]+ " ");
-}
+// 	while(j >= 0 && arr[j] > current){
+// 		arr[j+1] = arr[j];
+// 		j--;
+// 	}
+// 	   arr[j+1] = current;
+// }
+// for(int i=0; i<arr.length; i++){
+// 	System.out.print(arr[i]+ " ");
+// }
 
 
-//  ..........MERGE SORT.....(devide and conquer)...
+// //  ..........MERGE SORT.....(devide and conquer)...
 
-public class Main
-{
-    public static void conquer(int arr[], int si, int mid, int ei){
-        int merged[] = new int[ei - si + 1];
-        int idx1 = si;
-        int idx2 = mid+1;
-        int x = 0;
+// public class Main
+// {
+//     public static void conquer(int arr[], int si, int mid, int ei){
+//         int merged[] = new int[ei - si + 1];
+//         int idx1 = si;
+//         int idx2 = mid+1;
+//         int x = 0;
         
-        while(idx1 <= mid && idx2 <= ei ){
-            if(arr[idx1] <= arr[idx2]){
-                merged[x++] = arr[idx1++];
-            }
-            else {
-                merged[x++] = arr[idx2++];
-            }
-        }
-        while(idx1 <= mid){
-            merged[x++] = arr[idx1++]; 
-        }
+//         while(idx1 <= mid && idx2 <= ei ){
+//             if(arr[idx1] <= arr[idx2]){
+//                 merged[x++] = arr[idx1++];
+//             }
+//             else {
+//                 merged[x++] = arr[idx2++];
+//             }
+//         }
+//         while(idx1 <= mid){
+//             merged[x++] = arr[idx1++]; 
+//         }
         
-        while(idx2 <= ei){
-             merged[x++] = arr[idx2++];
-        }
+//         while(idx2 <= ei){
+//              merged[x++] = arr[idx2++];
+//         }
         
-        for(int i=0, j=si; i<merged.length; i++, j++){
-            arr[j] = merged[i];
-        }
+//         for(int i=0, j=si; i<merged.length; i++, j++){
+//             arr[j] = merged[i];
+//         }
         
-    }
-    public static void devide(int arr[], int si, int ei){
-        if(si >= ei){
-            return;
-        }
-        int mid = si + (ei - si)/2;
-        devide(arr , si, mid);
-        devide(arr, mid+1, ei);
-        conquer(arr, si, mid, ei);
+//     }
+//     public static void devide(int arr[], int si, int ei){
+//         if(si >= ei){
+//             return;
+//         }
+//         int mid = si + (ei - si)/2;
+//         devide(arr , si, mid);
+//         devide(arr, mid+1, ei);
+//         conquer(arr, si, mid, ei);
         
-    }
+//     }
     
-	public static void main(String[] args) {
-	  int arr[] = {3,7,34,9,7,8};
-	  int n =  arr.length;
-	  devide(arr, 0, n-1);
+// 	public static void main(String[] args) {
+// 	  int arr[] = {3,7,34,9,7,8};
+// 	  int n =  arr.length;
+// 	  devide(arr, 0, n-1);
 	  
-	  for(int i=0; i<n; i++){
-	      System.out.print(arr[i] + " ");
-	  }
-	  System.out.println();
-	}
-}
+// 	  for(int i=0; i<n; i++){
+// 	      System.out.print(arr[i] + " ");
+// 	  }
+// 	  System.out.println();
+// 	}
+// }
 
-// ..........QICK SORT....................
-import java.util.*;
-public class Main
-{
-    public static int partition(int arr[], int si, int ei){
-        int pivot = arr[ei];
-        int i = si-1;
-        for(int j=si; j<ei; j++){
-            if(arr[j] < pivot){
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-           int temp = arr[i+1];
-           arr[i+1]  = arr[ei];
-           arr[ei] = temp;
-           return i+1;
-    }
+// // ..........QICK SORT....................
+// import java.util.*;
+// public class Main
+// {
+//     public static int partition(int arr[], int si, int ei){
+//         int pivot = arr[ei];
+//         int i = si-1;
+//         for(int j=si; j<ei; j++){
+//             if(arr[j] < pivot){
+//                 i++;
+//                 int temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//            int temp = arr[i+1];
+//            arr[i+1]  = arr[ei];
+//            arr[ei] = temp;
+//            return i+1;
+//     }
     
-    public static void quicksort(int arr[], int si, int ei){
-        if(si < ei){
-            int pidx = partition(arr , si, ei);
-        quicksort(arr,si, pidx-1);
-        quicksort(arr, pidx+1, ei);
-        }
-    }
-	public static void main(String[] args) {
-		int arr[] = {5,2,7,8,6,9};
-		int n = arr.length;
-		quicksort(arr , 0, n-1);
-// 		print..
-      for(int i=0; i<n; i++){
-          System.out.print(arr[i] + " ");
-      }
-      System.out.println();
-	}
-}
+//     public static void quicksort(int arr[], int si, int ei){
+//         if(si < ei){
+//             int pidx = partition(arr , si, ei);
+//         quicksort(arr,si, pidx-1);
+//         quicksort(arr, pidx+1, ei);
+//         }
+//     }
+// 	public static void main(String[] args) {
+// 		int arr[] = {5,2,7,8,6,9};
+// 		int n = arr.length;
+// 		quicksort(arr , 0, n-1);
+// // 		print..
+//       for(int i=0; i<n; i++){
+//           System.out.print(arr[i] + " ");
+//       }
+//       System.out.println();
+// 	}
+// }

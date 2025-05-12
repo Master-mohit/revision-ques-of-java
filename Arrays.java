cee2844c0c759c9ab42b1f1ed5ea9b29b212908a
Master-mohit <mohitmansigupta9098@gmail.com>
2025-05-12 16:29:05 +0530

    sorted separte zero or non zero

--- Arrays.java
+++ Arrays.java
@@ -390,6 +390,22 @@ public class Arrays {
 // }
 // System.out.println(Arrays.toString(arr));
 
+//.............................Write a Java program to separate zeros from non-zeros sorted...............
+// int arr[] = {1,0,4,8,0,4,0,0,5,2}; 
+// int index = 0;
+// for(int i=0; i<arr.length; i++){
+//     if(arr[i] != 0){
+//         arr[index] = arr[i];
+//         index++;
+//     }
+// }
+//  Arrays.sort(arr, 0, index);
+ 
+// while(index < arr.length){
+//     arr[index] = 0;
+//     index++;
+// }
+// System.out.println(Arrays.toString(arr));
 
  // Write a Java program to separate zeros from non-zeros 
 // in an integer array?
